97224151100a3947e26bbeb3aa62311e61e6634d
ymonsalvez-maker <ymonsalvez@gmail.com>
2026-03-29 19:09:46 -0300

    agregar pedido

--- script.js
+++ script.js
@@ -1,22 +1,53 @@
-const cards = document.querySelectorAll(".card");
 const containers = document.querySelectorAll(".card-container");
 
 let draggedCard = null;
 
-cards.forEach(card => {
-  card.addEventListener("dragstart", () => {
-    draggedCard = card;
-  });
-});
-
 containers.forEach(container => {
-  container.addEventListener("dragover", (e) => {
-    e.preventDefault();
-  });
-
-  container.addEventListener("drop", () => {
-    if (draggedCard) {
-      container.appendChild(draggedCard);
-    }
-  });
+container.addEventListener("dragover", (e) => {
+e.preventDefault();
 });
+
+container.addEventListener("drop", () => {
+if (draggedCard) {
+container.appendChild(draggedCard);
+}
+});
+});
+
+function crearPedido(){
+document.getElementById("formulario").style.display = "block";
+}
+
+function guardarPedido(){
+
+const ref = document.getElementById("referencia").value;
+const club = document.getElementById("club").value;
+const estado = document.getElementById("estado").value;
+const fecha = document.getElementById("fecha").value;
+
+const card = document.createElement("div");
+card.className = "card";
+card.draggable = true;
+
+card.innerHTML = `
+<strong>Pedido ${ref}</strong> <br>
+Club: ${club} <br>
+Estado: Nuevo <br>
+Entrega: ${fecha}
+`;
+
+activarDrag(card);
+
+document.getElementById(estado).appendChild(card);
+
+document.getElementById("formulario").style.display = "none";
+
+}
+
+function activarDrag(card){
+
+card.addEventListener("dragstart", () => {
+draggedCard = card;
+});
+
+}
